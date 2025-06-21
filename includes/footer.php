775bd2edae6e0fdec244b1b9483b26f4d2d63849
Mark Vincent Tariman <tarimanmarkvincent@gmail.com>
2025-06-21 11:14:09 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer bg-transparent text-center py-2" style="position: fixed; width: 100%; bottom: 0; height: 10%; z-index: 100;">
+    <div class="container-fluid text-color: light">
+        <p class="text-light">&copy; Copyright. All rights reserved. 2024</p>
+        <p class="text-light"><b>Ronyx Trading Engineering Services</b></p>       
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ includes/navbar.php
@@ -0,0 +1,28 @@
+<div class="bg-secondary sticky-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg navbar-secondary ">
+                    <div class="container-fluid ">
+                    <a class="navbar-brand text-light" href="index.php"><b>Ronyx Trading</b></a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-light ">
+                        <li class="nav-item">
+                        <a class="nav-link active text-light" aria-current="page" href="signup.php"><b>Sign Up</b></a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active text-light" aria-current="page" href="products.php"><b>Products & Services</b></a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
